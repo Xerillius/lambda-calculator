cb7c432574c9db1226d7b98683dae017c611bfcb
Chris <caquinn.86@gmail.com>
2019-09-24 17:34:38 -0700

    finished assignment, attempted stretch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import Display from "./components/DisplayComponents/Display.js";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials.js";
+import Numbers from './components/ButtonComponents/NumberButtons/Numbers.js';
+import Operators from './components/ButtonComponents/OperatorButtons/Operators.js';
 import "./App.css";
+
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -7,6 +12,11 @@ import "./App.css";
 import Logo from "./components/DisplayComponents/Logo";
 
 function App() {
+  const [numString, setNumString] = useState("0");
+  const [temp, setTemp] = useState(0);
+  const [op, setOp] = useState();
+  const [done, setDone] = useState(false);
+
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
@@ -18,6 +28,29 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display num={numString} />
+        <div className="button-container">
+          <div className="special-numbers">
+            <Specials 
+              numString={numString} setNumString={setNumString}
+              temp={temp} setTemp={setTemp}
+              op={op} setOp={setOp}
+              done={done} setDone={setDone}
+            />
+            <Numbers
+              numString={numString} setNumString={setNumString}
+              done={done} setDone={setDone}
+            />
+          </div>
+          <div className="operators">
+            <Operators
+              numString={numString} setNumString={setNumString}
+              temp={temp} setTemp={setTemp}
+              op={op} setOp={setOp}
+              done={done} setDone={setDone}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,32 @@
 import React from "react";
 
-const SpecialButton = () => {
+export default function SpecialButton(props) {
+  function special() {
+    if(props.buttonValue === 'C') {
+      props.setNumString("0");
+      props.setTemp(0);
+      props.setOp();
+      props.setDone(false);
+    }
+    if(props.buttonValue === '+/-') {
+      let temp = props.numString.split('');
+      if(temp[0] === '-') {
+        temp.shift();
+        props.setNumString(temp.join(''));
+      } else {
+        props.setNumString('-' + props.numString);
+      }
+    }
+  }
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button
+        className="button special"
+        onClick={special}
+      >
+        <p>{props.buttonValue}</p>
+      </button>
     </>
   );
 };
